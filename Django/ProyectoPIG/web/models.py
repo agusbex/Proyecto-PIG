fdf9c9b67e7e4e009fa460c5f7dd99d8584ad735
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2024-06-10 19:13:29 -0300

    Update models.py

--- Django/ProyectoPIG/web/models.py
+++ Django/ProyectoPIG/web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class formulario(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre")
+    email = models.EmailField(verbose_name="Email") 
+    mensaje = models.TextField(verbose_name="mensaje")
+    
