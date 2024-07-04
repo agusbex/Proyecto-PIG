0cdf0b74583b9ee63987832ac44c518b90af373f
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2024-07-04 09:21:11 -0300

    Update admin.py

--- Django/ProyectoPIG/web/admin.py
+++ Django/ProyectoPIG/web/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+from .models import Contacto, Orden
 
+@admin.register(Contacto)
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'fecha')
+
+@admin.register(Orden)
+class OrdenAdmin(admin.ModelAdmin):
+    list_display = ('numero_orden', 'dni', 'estado', 'fecha_ingreso')
