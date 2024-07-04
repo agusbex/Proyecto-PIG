94f6c57659056d80e02616f5c8940a92c60186f4
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2024-07-04 09:20:49 -0300

    Update models.py

--- Django/ProyectoPIG/web/models.py
+++ Django/ProyectoPIG/web/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
-class formulario(models.Model):
-    nombre = models.CharField(max_length=100, verbose_name="Nombre")
-    email = models.EmailField(verbose_name="Email") 
-    mensaje = models.TextField(verbose_name="mensaje")
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    mensaje = models.TextField()
+    fecha = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre
     
+class Orden(models.Model):
+    numero_orden = models.CharField(max_length=10, unique=True)
+    dni = models.CharField(max_length=8)
+    estado = models.CharField(max_length=50)
+    descripcion = models.TextField(null=True, blank=True)
+    fecha_ingreso = models.DateField()
+
+    def __str__(self):
+        return self.numero_orden
+
