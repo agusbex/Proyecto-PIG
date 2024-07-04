9f4d521a871996a0dd4f311b0bd0a1ec25692b7f
yamiilaalejandra <136135024+yamiilaalejandra@users.noreply.github.com>
2024-07-04 09:21:14 -0300

    Update settings.py

--- Django/ProyectoPIG/ProyectoPIG/settings.py
+++ Django/ProyectoPIG/ProyectoPIG/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
+AUTH_USER_MODEL = 'auth.User'
